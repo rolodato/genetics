aceb96706c9a352e210e1b48c8031a9381cb4165
Rodrigo López Dato <rodrigo@auth0.com>
2015-04-28 23:32:07 -0300

    Use random default for xop in OnePointCrossover

--- src/main/scala/rolodato/genetics/OnePointCrossover.scala
+++ src/main/scala/rolodato/genetics/OnePointCrossover.scala
@@ -1,10 +1,10 @@
 package rolodato.genetics
 
 import scala.language.postfixOps
+import scala.util.Random
 
 object OnePointCrossover {
-  // TODO Allow passing Random as crossover value
-  def cross(i1: Individual, i2: Individual, xop: Int): Set[Individual] = {
+  def cross(i1: Individual, i2: Individual, xop: Int = Random.nextInt()) = {
     assert(i1.length == i2.length, "individuals must have equal length strings")
     val child1 = ((i1 string) take xop) ++ ((i2 string) drop xop)
     val child2 = ((i2 string) take xop) ++ ((i1 string) drop xop)
