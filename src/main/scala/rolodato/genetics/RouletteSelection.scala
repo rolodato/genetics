dca7ce29a8079dbf10bc36e71b7dbeb28f162d52
Rodrigo López Dato <rodrigo@auth0.com>
2015-04-28 23:55:27 -0300

    Allow passing random R value to RouletteSelection

--- src/main/scala/rolodato/genetics/RouletteSelection.scala
+++ src/main/scala/rolodato/genetics/RouletteSelection.scala
@@ -4,18 +4,18 @@ import scala.language.postfixOps
 import scala.util.Random
 
 object RouletteSelection {
-  def select(population: List[Individual]): Individual = {
+  def select(pop: List[Individual], r: Double = Random.nextDouble()): Individual = {
     // TODO How to test this? Generators + proportions of results?
-    val fits = normalize(population map (_.fitness))
+    val fits = normalize(pop map (_.fitness))
     // Sort the population in the same order as the normalized fitness values
-    val popWithFits = population zip fits sortBy (-_._2)
+    val popWithFits = pop zip fits sortBy (-_._2)
     // Separate population and fitness values after sorting
     val sortedPop = popWithFits.unzip._1
     val sortedFits = popWithFits.unzip._2
     val accFits = accumulatedFitness(sortedFits)
     // TODO Make random generator configurable to allow reproducible results
     // find returns Option, but this should never fail
-    (sortedPop zip accFits find (_._2 > Random.nextDouble)).get._1
+    (sortedPop zip accFits find (_._2 > r)).get._1
   }
 
   def accumulatedFitness(fits: List[Double]): List[Double] = {
