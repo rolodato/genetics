adf5f78ba7eceecab4015596d18ed60b75a4a28c
Rodrigo López Dato <rodrigo@auth0.com>
2015-05-19 00:41:33 -0300

    Use arrow parameter for xop
    This lets crossAll choose a different xop for each iteration, instead of
    crossing the entire population with the same xop each time.

--- src/main/scala/rolodato/genetics/OnePointCrossover.scala
+++ src/main/scala/rolodato/genetics/OnePointCrossover.scala
@@ -3,14 +3,14 @@ package rolodato.genetics
 import scala.language.postfixOps
 
 object OnePointCrossover {
-  def cross[T](i1: Individual[T], i2: Individual[T], xop: Int): List[Individual[T]] = {
+  def cross[T](i1: Individual[T], i2: Individual[T], xop: => Int): List[Individual[T]] = {
     assert(i1.length == i2.length, "individuals must have equal length strings")
     val child1 = ((i1 string) take xop) ++ ((i2 string) drop xop)
     val child2 = ((i2 string) take xop) ++ ((i1 string) drop xop)
     List(i1.copy(child1), i2.copy(child2))
   }
 
-  def crossAll[T](pop: List[Individual[T]], xop: Int): List[Individual[T]] = {
+  def crossAll[T](pop: List[Individual[T]], xop: => Int): List[Individual[T]] = {
     pop match {
       case Nil => Nil
       case List(x) => List(x)
