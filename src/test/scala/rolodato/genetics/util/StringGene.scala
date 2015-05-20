feafaf9b72ed52d1f185d67009d7aea8c545555e
Rodrigo López Dato <rodrigo@auth0.com>
2015-05-20 17:45:55 -0300

    Only calculate string once, and when needed

--- src/test/scala/rolodato/genetics/util/StringGene.scala
+++ src/test/scala/rolodato/genetics/util/StringGene.scala
@@ -3,7 +3,7 @@ package rolodato.genetics.util
 import rolodato.genetics.Gene
 
 class StringGene(val string: List[Int]) extends Gene {
-  def fitness: Double = string.length
+  lazy val fitness: Double = string.length
 
   def copy(newString: List[Int]): Gene = new StringGene(newString)
 }
