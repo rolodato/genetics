7c5d7d9b6acc671355b0dbe089bf31dd94437bc7
Rodrigo López Dato <rodrigo@auth0.com>
2015-05-20 01:33:07 -0300

    Remove useless type parameter

--- src/main/scala/rolodato/genetics/Crossover.scala
+++ src/main/scala/rolodato/genetics/Crossover.scala
@@ -2,7 +2,8 @@ package rolodato.genetics
 
 trait Crossover {
   def cross(parent1: Gene, parent2: Gene): List[Gene]
-  def crossAll[T](population: List[Gene]): List[Gene] = {
+
+  def crossAll(population: List[Gene]): List[Gene] = {
     population match {
       case Nil => Nil
       case List(x) => List(x)
