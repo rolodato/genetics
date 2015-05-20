c1fc2b30604b816cccfdd7e32ab13b98e7d8986d
Rodrigo López Dato <rodrigo@auth0.com>
2015-05-20 01:29:28 -0300

    Remove useless validation

--- src/main/scala/rolodato/genetics/impl/OnePointCrossover.scala
+++ src/main/scala/rolodato/genetics/impl/OnePointCrossover.scala
@@ -13,7 +13,6 @@ trait OnePointCrossover extends Crossover {
   def cross(parent1: Gene, parent2: Gene): List[Gene] = {
     require(parent1.length == parent2.length, "gene lengths must be equal")
     val xop: Int = abs(crossoverPoint % parent1.length)
-    require(0 <= xop && xop <= parent1.length, "xover point must be valid")
     val child1 = ((parent1 string) take xop) ++ ((parent2 string) drop xop)
     val child2 = ((parent2 string) take xop) ++ ((parent1 string) drop xop)
     List(parent1.copy(child1), parent2.copy(child2))
