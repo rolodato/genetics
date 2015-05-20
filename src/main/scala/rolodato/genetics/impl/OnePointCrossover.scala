24565f2067c57b2e3ee59d878b471d5c4e67bfcf
Rodrigo López Dato <rodrigo@auth0.com>
2015-05-20 01:16:03 -0300

    Use random xop in OnePointCrossover by default

--- src/main/scala/rolodato/genetics/impl/OnePointCrossover.scala
+++ src/main/scala/rolodato/genetics/impl/OnePointCrossover.scala
@@ -3,14 +3,16 @@ package rolodato.genetics.impl
 import rolodato.genetics.{Crossover, Gene}
 
 import scala.language.postfixOps
+import scala.math.abs
+import scala.util.Random
 
 trait OnePointCrossover extends Crossover {
 
-  def crossoverPoint: Int
+  def crossoverPoint: Int = Random.nextInt()
 
   def cross(parent1: Gene, parent2: Gene): List[Gene] = {
     require(parent1.length == parent2.length, "gene lengths must be equal")
-    val xop: Int = crossoverPoint
+    val xop: Int = abs(crossoverPoint % parent1.length)
     require(0 <= xop && xop <= parent1.length, "xover point must be valid")
     val child1 = ((parent1 string) take xop) ++ ((parent2 string) drop xop)
     val child2 = ((parent2 string) take xop) ++ ((parent1 string) drop xop)
@@ -21,6 +23,6 @@ trait OnePointCrossover extends Crossover {
 
 object OnePointCrossover {
   def apply(xop: Int) = new OnePointCrossover {
-    def crossoverPoint: Int = xop
+    override def crossoverPoint: Int = xop
   }
 }
