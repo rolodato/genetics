d04e70124f270bf2a8d910b50d356bfee91cc177
Rodrigo López Dato <rodrigo@auth0.com>
2015-05-27 20:32:42 -0300

    Sort by descending fitness

--- shared/src/main/scala/rolodato/genetics/GeneticResult.scala
+++ shared/src/main/scala/rolodato/genetics/GeneticResult.scala
@@ -2,6 +2,6 @@ package rolodato.genetics
 
 trait GeneticResult {
   val finalPopulation: List[Gene]
-  def fittest: Gene = finalPopulation.sortBy(_.fitness).head
+  def fittest: Gene = finalPopulation.sortBy(-_.fitness).head
   val fitnessEvolution: List[Double]
 }
